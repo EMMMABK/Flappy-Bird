100de8b26e71f29f8d2b952a31c47c5a71c890de
EMMMABK <adilbikiev2@gmail.com>
2023-12-05 21:20:07 +0600

    add FuncALl

--- js/script.js
+++ js/script.js
@@ -36,9 +36,11 @@ pipe[0] = {
     y: 0
 };
 
+let score = 0;
 let xPos = 10;
 let yPos = 150;
 let grav = 1.5;
+
 
 function draw() {
     ctx.drawImage(bg, 0, 0);
@@ -54,14 +56,22 @@ function draw() {
             });
         }    
         
-        if(xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x + pipeUp.width && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap)){
+        if(xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x + pipeUp.width && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height){
             location.reload()    
+        }
+        if(pipe[i].x == 5){
+            score++;
         }
     }
     ctx.drawImage(fg, 0, cvs.height - fg.height);
     ctx.drawImage(bird, xPos, yPos);
 
     yPos += grav;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "24px Verdana";
+    ctx.fillText(`Счет: ${score}`, 10, cvs.height - 20);
+    
     requestAnimationFrame(draw);
 }
 
